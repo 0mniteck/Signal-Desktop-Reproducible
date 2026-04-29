d89a5b8d628fe22e7a6409a80a4016874a88e711
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2026-04-29 15:53:50 -0700

    Modify git fetch command to use variable for shallow
    Signed-off-by: Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>

--- buildscript.sh
+++ buildscript.sh
@@ -576,8 +576,10 @@ if [[ \"$TESTS\" != *SKIP_LOGIN* ]]; then
   git remote remove origin && git remote add origin git@\$MODULE:\$REPO/\$PROJECT.git
   git-lfs install && git reset --hard && git clean -xfd
 
+  read -r -p "git fetch --unshallow? (y/n): " ans; if [[ "$ans" == "y" ]]; then shallow=--unshallow ; fi;
   confirm 'git fetch - git@ssh (twice)' && echo 'Starting Git fetch...'
-  git fetch --unshallow 2>> $nulled || true
+  git fetch \$shallow 2>> $nulled || true
+
   confirm 'git pull - git@ssh' && echo 'Starting Git pull...'
   git pull \$(git remote -v | awk '{ print \$2 }' | tail -n 1) \$(git rev-parse --abbrev-ref HEAD)
 
